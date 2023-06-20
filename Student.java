f7cd31970dc6f20a1dec0d5e919c1d065b8295b4
Ashish Kumar <104408789+ashisha162@users.noreply.github.com>
2023-06-20 08:44:56 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Student  //class is a blue print of an object//
+
+{
+	String name;
+	int marks;
+	if(marks>40)
+	{System.out.println("Passed");
+	}
+	else
+	{
+	System.out.println("Failed");
+public class Control_Flow
+{
+public static void main(String args[])
+{
+Student s1=new Student();
+s1.name="Ashish";
+s1.marks=50;
+Student s2=new Student();
+s2.name="Shyam";
+s1.marks=72;
+System.out.println(s1.Student);
+System.out.pritln(s2.Student);
+}
+}
---
+++ Students.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Students
+{
+	int rollNo; //camel casing
+	String name;
+	public static void main(String args[])
+	{
+		Students s1=new Students();
+		Students s2=new Students();
+		s1.name="Ram";
+		s1.rollNo=21;
+	s2.name="Lakshman";
+	s2.rollNo=22;
+	System.out.println(s1.name);
+	System.out.println(s1.rollNo);
+	
+	System.out.println(s2.name);
+	System.out.println(s2.rollNo);
+	}
+}
+	
+		
